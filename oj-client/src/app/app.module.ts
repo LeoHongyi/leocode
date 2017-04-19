ca12e54c6ee83049287a0789dafac41fbe10679c
Leohongyi <wenhualiu.liu@gmail.com>
2017-04-19 17:08:34 -0500

    upload leocode to github

---
+++ oj-client/src/app/app.module.ts
@@ -0,0 +1,58 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { routing } from "./app.routes";
+
+import { AppComponent } from './app.component';
+import { ProblemListComponent } from './components/problem-list/problem-list.component';
+
+import {DataService} from "./services/data.service";
+import {AuthService} from "./services/auth.service";
+import { AuthGuardService } from "./services/auth-guard.service";
+import { ProblemDetailComponent } from './components/problem-detail/problem-detail.component';
+import { CollaborationService } from "./services/collaboration.service";
+import { InputService } from "./services/input.service";
+import { NewProblemComponent } from './components/new-problem/new-problem.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { EditorComponent } from './components/editor/editor.component';
+import { SearchPipe } from './pipes/search.pipe'
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProblemListComponent,
+    ProblemDetailComponent,
+    NewProblemComponent,
+    NavbarComponent,
+    ProfileComponent,
+    EditorComponent,
+    SearchPipe
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpModule,
+    routing
+  ],
+  providers: [{
+  provide: "data",
+  useClass: DataService
+},{
+    provide: "auth",
+    useClass: AuthService
+  },{
+    provide: "authGuard",
+    useClass: AuthGuardService
+  }, {
+    provide: "collaboration",
+    useClass: CollaborationService
+  }, {
+    provide: "input",
+    useClass: InputService
+  }],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
